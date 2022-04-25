6a2e871d9f277b1392a8ba3360f8bb64e62c0abc
arora <aroramadhav65@gmail.com>
2022-04-25 13:03:14 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Center(
-        child: Text('Calling Interface'),
+        child: Text('Calling Interface',),
       ),
     );
   }
