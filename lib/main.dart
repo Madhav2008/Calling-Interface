25b6bd686ff39bccb21e1bcd125373348692701d
arora <aroramadhav65@gmail.com>
2022-04-25 12:50:17 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
