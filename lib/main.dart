e8bdb4ca0e91debd00aede50abdef43378cb6456
arora <aroramadhav65@gmail.com>
2022-04-25 12:51:49 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calling',
+      title: 'Calling Interface',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
