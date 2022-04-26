6068008ef55ca567b64a7bfc92598692797fca1c
arora <aroramadhav65@gmail.com>
2022-04-26 13:25:45 +0530

    Update

--- lib/screens/dialScreen/dial_screen.dart
+++ lib/screens/dialScreen/dial_screen.dart
@@ -1,7 +1,6 @@
 import 'package:calling_interface_flutter/constants.dart';
 import 'package:calling_interface_flutter/size_config.dart';
 import 'package:flutter/material.dart';
-
 import 'components/body.dart';
 
 class DialScreen extends StatelessWidget {
