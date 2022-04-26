1eb875440999167bddc900afdb804de1363ccc0b
arora <aroramadhav65@gmail.com>
2022-04-26 13:29:01 +0530

    Update

--- lib/screens/groupCall/group_call_screen.dart
+++ lib/screens/groupCall/group_call_screen.dart
@@ -1,5 +1,5 @@
 import 'package:calling_interface_flutter/components/rounded_button.dart';
-import 'package:calling/constants.dart';
+import 'package:calling_interface_flutter/constants.dart';
 import 'package:calling/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
