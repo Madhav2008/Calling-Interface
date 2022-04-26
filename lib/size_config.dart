7d67edaef5914e827fcd5480507a9eee41d111c0
arora <aroramadhav65@gmail.com>
2022-04-26 13:10:22 +0530

    Update

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SizeConfig {
-  static MediaQueryData _mediaQueryData;
+  required static MediaQueryData _mediaQueryData;
   static double screenWidth;
   static double screenHeight;
   static double defaultSize;
