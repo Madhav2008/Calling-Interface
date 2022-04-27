3d21b3faf446aaffa1d36cab690812bb16ae4cbc
arora <aroramadhav65@gmail.com>
2022-04-27 15:20:50 +0530

    Update

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -1,20 +1,5 @@
 import 'package:flutter/material.dart';
 
-class SizeConfig {
-  static MediaQueryData _mediaQueryData;
-  static double screenWidth;
-  static double screenHeight;
-  static double defaultSize;
-  static Orientation orientation;
-
-  void init(BuildContext context) {
-    _mediaQueryData = MediaQuery.of(context);
-    screenWidth = _mediaQueryData.size.width;
-    screenHeight = _mediaQueryData.size.height;
-    orientation = _mediaQueryData.orientation;
-  }
-}
-
 double getProportionateScreenHeight(double inputHeight, context) {
   double screenHeight = MediaQuery.of(context).size.height;
   return (inputHeight / 896.0) * screenHeight;
