e741625d3fb316595376f92c9371448aec7089c8
arora <aroramadhav65@gmail.com>
2022-05-03 12:47:01 +0530

    Update

--- lib/screens/dialScreen/dial_screen.dart
+++ lib/screens/dialScreen/dial_screen.dart
@@ -6,7 +6,7 @@ import 'components/body.dart';
 class DialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
+    
     return Scaffold(
       backgroundColor: kBackgoundColor,
       body: Body(),
