cd00d6878d5f2f9d8462656674653565e60fe676
arora <aroramadhav65@gmail.com>
2022-05-03 15:44:33 +0530

    'Update'

--- lib/screens/groupCall/group_call_screen.dart
+++ lib/screens/groupCall/group_call_screen.dart
@@ -57,7 +57,7 @@ class GroupCallScreen extends StatelessWidget {
                 color: Color(0xFF2C384D),
                 iconColor: Colors.white,
                 size: 48,
-                iconSrc: "../assets/icons/Icon Repeat.svg",
+                iconSrc: "../assets/icons/Icon_Repeat.svg",
                 press: () {},
               ),
             ],
