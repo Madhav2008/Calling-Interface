8534b068dce4f9f55942973e042ea7a1dac855bf
arora <aroramadhav65@gmail.com>
2022-05-03 12:48:00 +0530

    Update

--- lib/screens/groupCall/group_call_screen.dart
+++ lib/screens/groupCall/group_call_screen.dart
@@ -8,7 +8,6 @@ import 'components/body.dart';
 class GroupCallScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SizeConfig().init(context);
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: buildAppBar(),
