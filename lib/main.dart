547a24c8634affc385c751148f3a3ae3cdebec17
arora <aroramadhav65@gmail.com>
2022-05-03 12:50:33 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Calling',
       theme: ThemeData(
         fontFamily: "SF UI",
         visualDensity: VisualDensity.adaptivePlatformDensity,
