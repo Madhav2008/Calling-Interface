2de4f0881e0578feec7d8a164231f5cf36d4027a
arora <aroramadhav65@gmail.com>
2022-05-03 12:49:56 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:calling_interface/screens/dialScreen/dial_screen.dart';
+import 'package:calling_interface_flutter/screens/dialScreen/dial_screen.dart';
 import 'package:calling/screens/groupCall/group_call_screen.dart';
 import 'package:flutter/material.dart';
 
