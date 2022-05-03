6d608130329760b3e62cd75b8e07f5f2afd8b254
arora <aroramadhav65@gmail.com>
2022-05-03 12:47:11 +0530

    Update

--- lib/screens/dialScreen/dial_screen.dart
+++ lib/screens/dialScreen/dial_screen.dart
@@ -1,5 +1,4 @@
 import 'package:calling_interface_flutter/constants.dart';
-import 'package:calling_interface_flutter/size_config.dart';
 import 'package:flutter/material.dart';
 import 'components/body.dart';
 
